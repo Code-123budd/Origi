decd139aee9f0c67c6e6d7458537ef94b8ebdb07
tarush <tarushruhela1234@gmail.com>
2025-07-19 11:00:24 +0530

    update cors func

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
-const allowedOrigins = [process.env.CLIENT_URL , 'http://localhost:5173/'];
+// const allowedOrigins = [process.env.CLIENT_URL ];
 
 // Middleware
 // app.use(cors({
@@ -29,14 +29,25 @@ const allowedOrigins = [process.env.CLIENT_URL , 'http://localhost:5173/'];
 //   credentials: true,
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // }));
-app.use(cors())
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://new-video-call.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userroute);
-app.get('/', (req, res) => {
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userroute);
+app.get("/", (req, res) => {
   res.json("API is running");
 });
 
@@ -51,7 +62,7 @@ app.get('/', (req, res) => {
       cors: {
         origin: allowedOrigins[0],
         methods: ["GET", "POST"],
-      }
+      },
     });
 
     console.log("✅ Socket.io Initialized with CORS");
@@ -75,50 +86,51 @@ app.get('/', (req, res) => {
           onlineUser.push({
             userId: user.id,
             name: user.name,
-            socketId: socket.id
+            socketId: socket.id,
           });
         }
         io.emit("online-users", onlineUser);
       });
-      
-      socket.on("callToUser",(data)=>{
+
+      socket.on("callToUser", (data) => {
         //console.log("Incoming call from",data);
         //console.log("Online users:", onlineUser);
-        const call=onlineUser.find((user)=>user.userId === data.callToUserId);
-        if(!call ){
-          socket.emit("userUnavailable",{message:`User Is Offline`});
+        const call = onlineUser.find(
+          (user) => user.userId === data.callToUserId
+        );
+        if (!call) {
+          socket.emit("userUnavailable", { message: `User Is Offline` });
           return;
         }
         //emit an event to
-        io.to(call.socketId).emit("callToUser",{
-            signal:data.signalData,
-            from:data.from,
-            name:data.name,
-            email:data.email,
-            profilepic:data.profilepic
-        })
-      })
+        io.to(call.socketId).emit("callToUser", {
+          signal: data.signalData,
+          from: data.from,
+          name: data.name,
+          email: data.email,
+          profilepic: data.profilepic,
+        });
+      });
 
-      socket.on("answeredCall",(data)=>{
-        io.to(data.to).emit("callAccepted",{
+      socket.on("answeredCall", (data) => {
+        io.to(data.to).emit("callAccepted", {
+          signal: data.signal,
+          from: data.from,
+        });
+      });
 
-        signal:data.signal,
-        from:data.from
-        })
-      })
+      socket.on("call-ended", (data) => {
+        io.to(data.to).emit("callEnded", {
+          name: data.name,
+        });
+      });
 
-      socket.on("call-ended",(data)=>{
-        io.to(data.to).emit("callEnded",{
-          name:data.name,
-        })
-      })
-
-      socket.on("reject-call",(data)=>{
-        io.to(data.to).emit("callRejected",{
-          name:data.name,
-          profilepic:data.profilepic
-        })
-      })
+      socket.on("reject-call", (data) => {
+        io.to(data.to).emit("callRejected", {
+          name: data.name,
+          profilepic: data.profilepic,
+        });
+      });
       socket.on("disconnect", () => {
         const user = onlineUser.find((u) => u.socketId === socket.id);
         onlineUser = onlineUser.filter((u) => u.socketId !== socket.id);
@@ -127,11 +139,10 @@ app.get('/', (req, res) => {
         console.log("⚠️ Disconnected:", socket.id);
       });
     });
-    const PORT=process.env.PORT||3000;
+    const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => {
       console.log(`🚀 Server is running on port ${PORT}`);
     });
-
   } catch (error) {
     console.error("❌ App Crashed:", error.message);
     process.exit(1);
