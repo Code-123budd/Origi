73dd084605aaf6cf98f756d7d958ac6657ce71c0
tarush <tarushruhela1234@gmail.com>
2025-07-19 10:51:30 +0530

    update cors func

--- server/index.js
+++ server/index.js
@@ -17,18 +17,19 @@ const server = createServer(app);
 const allowedOrigins = [process.env.CLIENT_URL , 'http://localhost:5173/'];
 
 // Middleware
-app.use(cors({
-  origin: function (origin, callback) {
-    console.log(`req get from ${origin}`)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     console.log(`req get from ${origin}`)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
